fd5d8e93165a8f6cd714bb50a7adf19fd4ced577
PHVital <vital.ph.g@gmail.com>
2025-04-28 09:58:10 -0300

    Configurando Admin

--- contas/admin.py
+++ contas/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Cliente, ContaBancaria, Transacao
 
-# Register your models here.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'email')
+    search_fields = ('nome', 'cpf')
+
+@admin.register(ContaBancaria)
+class ContaBancariaAdmin(admin.ModelAdmin):
+    list_display = ('numero_conta', 'cliente', 'saldo')
+    search_fields = ('numero_conta', 'cliente__nome')
+
+@admin.register(Transacao)
+class TransacaoAdmin(admin.ModelAdmin):
+    list_display = ('conta', 'tipo', 'valor', 'data')
+    search_fields = ('conta__numero_conta',)
+    list_filter = ('tipo',)
